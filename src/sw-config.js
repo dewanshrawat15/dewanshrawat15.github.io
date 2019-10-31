a43ea9b3607bef94f99932d3925b97bf362ecf43
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-10-31 13:57:22 +0530

    Trying to add a starting page and offline page

--- src/sw-config.js
+++ src/sw-config.js
@@ -1,7 +1,10 @@
 module.exports = {
   staticFileGlobs: [
     'index.html',
+    'offline.html',
     'manifest.json',
     'dist/**.js',
   ],
+  navigateFallback: '/offline.html',
+  navigateFallbackWhitelist: [/^(?!.*\.js$|\/data\/).*/]
 };
