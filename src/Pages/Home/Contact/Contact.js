1f8086ce0a5ce9ff02fd9364dee7cdc6498878e3
Dewansh Rawat <dewanshrawat15@gmail.com>
2020-10-17 12:42:41 +0530

    Designed a base layout for the home page
    Base layout of the home page is ready. Have to work upon the Projects, Experience and Blogs section.

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -7,28 +7,63 @@ class Contact extends React.Component{
     render(){
         return(
             <div className="contact">
-                    <div className="container">
+                <div className="contact-laptop">
+                    <div className="container-fluid">
                         <div className="row">
                             <div className="col-md-7">
                                 <div className="row">
-                                    <div className="col-md-5 contact-title">
+                                    <div className="col-md-5 col-md-offset-1 contact-title">
                                         Contact Me
                                     </div>
                                 </div>
                                 <br /><br /><br />
                                 <div className="row">
-                                    <div className="col-md-10 contact-text">
+                                    <div className="col-md-10 col-md-offset-1 contact-text">
                                         Feel free to reach out to me using any of your favorite platforms.
                                     </div>
                                 </div>
                                 <br /><br />
-                                <Socials />
+                                <div className="col-md-offset-1">
+                                    <Socials />
+                                </div>
                             </div>
+                            <div className="col-md-5">
+                                <center>
+                                    <img src={contactMeVector} height="1200" width="1200" className="img-responsive" alt="Contact Me Vector" />
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact-mobile">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-5 contact-title text-center">
+                                Contact Me
+                            </div>
+                        </div>
+                        <br />
+                        <div className="row">
                             <div className="col-md-5">
                                 <img src={contactMeVector} height="1200" width="1200" className="img-responsive" alt="Contact Me Vector" />
                             </div>
                         </div>
+                        <br />
+                        <div className="row">
+                            <div className="col-md-10 contact-text text-center">
+                                Feel free to reach out to me using any of your favorite platforms.
+                            </div>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <div className="col-md-10">
+                                <center>
+                                    <Socials />
+                                </center>
+                            </div>
+                        </div>
                     </div>
+                </div>
             </div>
         );
     }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Contact from './Contact/Contact';
+import Footer from "./Footer/Footer";
 
 class Home extends React.Component{
     render(){
@@ -10,6 +11,7 @@ class Home extends React.Component{
                 <Hero />
                 <About />
                 <Contact />
+                <Footer />
             </>
         );
     }
