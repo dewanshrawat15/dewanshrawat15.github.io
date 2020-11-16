ab6ab8dc31b46f349a1c57d0e11a7b1f32e54843
Dewansh Rawat <dewanshrawat15@Dewanshs-MacBook-Air.local>
2020-11-16 13:07:02 +0530

    Updating manifest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
-// import {
-//   Switch,
-//   BrowserRouter as Router,
-//   Route
-// } from 'react-router-dom';
 import Switch from "react-router-dom/Switch";
 import BrowserRouter from "react-router-dom/BrowserRouter";
 import Route from "react-router-dom/Route";
-// import Redirect from "react-router-dom/Redirect";
 import Home from './Pages/Home/Home';
 import FourOFourPage from "./Pages/FourZeroFour/404";
 
