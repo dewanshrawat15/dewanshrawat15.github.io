774306926138a177ea2cbc7fa7387b14372103ed
Dewansh Rawat <dewanshrawat15@gmail.com>
2025-02-25 10:09:30 +0530

    created base layout with tailwind

--- src/api/context.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { defaultValue } from "../utils/constants";
-import { PortfolioWebsiteModel } from "./models";
-
-export const PortfolioContext = createContext<PortfolioWebsiteModel>(defaultValue);
