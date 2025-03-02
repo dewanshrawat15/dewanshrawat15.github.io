d15658e887cbe1af209300aee7c9be09ee75c433
Dewansh Rawat <dewanshrawat15@gmail.com>
2025-03-02 22:55:34 +0530

    adding some animations

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import { workingExperience } from "./constants";
 import { ExperienceItem } from "./experience-item";
+import { motion } from "motion/react";
 
 export const Experience = () => {
   const experienceItems = useMemo(
@@ -12,19 +13,31 @@ export const Experience = () => {
   );
 
   return (
-    <div id="experience">
-      <div className="flex px-12 py-4">
-        <div className="w-full md:w-3/5 py-5">
-          <h1 className="font-sans font-extralight text-gray-500 text-lg md:text-2xl xl:text-5xl">
-            Past Experience
-          </h1>
-          <div className="flex flex-col space-y-4 mt-10">
-            {experienceItems.map((experience, index) => (
-              <ExperienceItem experience={experience} key={index} />
-            ))}
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
+      <div id="experience">
+        <div className="flex px-12 py-4">
+          <div className="w-full md:w-3/5 py-5">
+            <h1 className="font-sans font-extralight text-gray-500 text-lg md:text-2xl xl:text-5xl">
+              Past Experience
+            </h1>
+            <div className="flex flex-col space-y-4 mt-10">
+              {experienceItems.map((experience, index) => (
+                <ExperienceItem experience={experience} key={index} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="flex flex-row px-12 py-7 text-sm text-gray-700 font-light">
+    <div className="mt-4 flex flex-row px-12 py-7 text-sm text-gray-700 font-light bg-gray-50">
       Made with <span className="px-1 text-red-600">&hearts;</span> by Dewansh
       Rawat
     </div>
