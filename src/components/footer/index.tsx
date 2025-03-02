267f2f9378fdadd4a0a5d121609ccd5f4420cd29
Dewansh Rawat <dewanshrawat15@gmail.com>
2025-03-02 21:53:46 +0530

    completed basic layout

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <div className="flex flex-row p-12 text-sm text-gray-700 font-light">
+      Made with <span className="px-1 text-red-600">&hearts;</span> by Dewansh
+      Rawat
+    </div>
+  );
+};
